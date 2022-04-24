91e78e88a49669c0c5a699f3f807dece203c5512
Odysseas Lamtzidis <odyslam@gmail.com>
2022-04-24 18:36:49 +0200

    feat: stream logs, add netdata overview, basic auth

--- caddy/stream-logs.sh
+++ caddy/stream-logs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
-
+message() {
+    echo
+    echo -----------------------------------------------------------------------------
+    echo -e "$@"
+    echo -----------------------------------------------------------------------------
+    echo
+}
 STREAM_FILE="/root/website/deviceLogs.txt"
 message "Streaming logs to '/data/caddy/website/balenaLogs.txt' which is accessible via typing 'IP:PORT/dashboard/balenaLogs.txt' in the browser"
 COUNTER=0
 curl -X POST -H "Content-Type: application/json" --no-buffer --data '{"follow":true,"all":false,"format":"short"}' "$BALENA_SUPERVISOR_ADDRESS/v2/journal-logs?apikey=$BALENA_SUPERVISOR_API_KEY" \
-  | grep -v "avahi-daemon.*|kernel|balenad.*|balena-supervisor.*" \
+  | grep -v "avahi-daemon.*|kernel.*|balenad.*|balena-supervisor.*" \
   | while read -r line ;
     do
       if [ "${COUNTER}" -eq "1000" ]; then
--- caddy/website/index.js
+++ caddy/website/index.js
@@ -5,14 +5,17 @@ window.onload = async () => {
   } else {
     const logsLoop = setInterval(loadLogs, 1000);
   };
+  NETDATA.options.current.stop_updates_when_focus_is_lost = false
 }
 async function loadLogs() {
-  const res = await fetch("deviceLogs.txt");
+  let myHeaders = new Headers();
+  let creds = btoa("CADDY_USERNAME:CADDY_PASSWORD");
+  myHeaders.append("Authorization", `basic ${creds}`);
+  const res = await fetch("deviceLogs.txt", {method: 'GET', headers: myHeaders});
   let logs = await res.text();
   const textarea = document.getElementById('logs');
   if (textarea != document.activeElement) {
     textarea.scrollTop = textarea.scrollHeight;
   }
   textarea.value = logs;
-
 }
