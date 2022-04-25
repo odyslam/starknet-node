f09a788258a8134322f60d41aa1a38e333780b82
Odysseas Lamtzidis <odyslam@gmail.com>
2022-04-25 11:43:59 +0200

    feat: move log streamer to it's own container

--- caddy/start-caddy.sh
+++ caddy/start-caddy.sh
@@ -7,15 +7,6 @@ message() {
     echo -----------------------------------------------------------------------------
     echo
 }
-# Set the hostname of the device using the balena supervisor API
-curl -f -s -X PATCH --header "Content-Type:application/json" \
-    --data '{"network": {"hostname": "starknet"}}' \
-    "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" \
-    && echo && echo "Starknet Node set hostname to 'starknet'. You can access the device by typing starknet.local on your browser" \
-    || echo "Starknet Node failed to set the hostname of the device. If you are not using balena but regular Docker, this is normal."
-
-# Start streaming logs to a file that is later loaded via the dashboard
-sh /usr/local/bin/stream-logs.sh &
 
 message "Starting Caddy with the following credentials:"
 echo "USERNAME: ${CADDY_USERNAME}"
