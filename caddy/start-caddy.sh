d28b8572444331c3d7d2cab1c8f9726c6dcbd3f4
Odysseas Lamtzidis <odyslam@gmail.com>
2022-04-25 20:09:48 +0200

    feat: reverse proxy logs, remove streamer

--- caddy/start-caddy.sh
+++ caddy/start-caddy.sh
@@ -17,3 +17,5 @@ export CADDY_PASSWORD_HASH=$(caddy hash-password --plaintext ${CADDY_PASSWORD})
 # Since the website is only loaded if basic AUTH is succesful, the plaintext credentials pose no additional security threat
 sed -i "s/CADDY_PASSWORD/${CADDY_PASSWORD}/g" /root/website/index.js && sed -i "s/CADDY_USERNAME/${CADDY_USERNAME}/g" /root/website/index.js
 caddy run --config /etc/caddy/Caddyfile
+# Used for debugging
+# tail -f /dev/null
--- caddy/website/index.js
+++ caddy/website/index.js
@@ -1,18 +1,44 @@
 window.onload = async () => {
-  const res = await fetch("deviceLogs.txt");
-  let logs = await res.text();
-  if (logs == "") {
-  } else {
-    const logsLoop = setInterval(loadLogs, 3000);
-  };
-  NETDATA.options.current.stop_updates_when_focus_is_lost = false;
-}
-async function loadLogs() {
-  const res = await fetch("../device/logs.txt", {method: 'GET'});
-  let logs = await res.text();
-  const textarea = document.getElementById('logs');
-  if (textarea != document.activeElement) {
-    textarea.scrollTop = textarea.scrollHeight;
+  try {
+    streamLogs();
+  } catch (error) {
+    console.error(error);
+    location.reload();
   }
-  textarea.value = logs;
+}
+
+
+async function streamLogs() {
+  const textarea = document.getElementById('logs');
+  fetch('../supervisor/logs', {method: 'POST'})
+    // Retrieve its body as ReadableStream
+    .then(response => {
+      const reader = response.body.getReader();
+      return new ReadableStream({
+        start(controller) {
+          return pump();
+          function pump() {
+            return reader.read().then(({done, value}) => {
+              // When no more data needs to be consumed, close the stream
+              if (done) {
+                controller.close();
+                if (textarea.value == "") {
+                  textarea.value = "Could not stream logs. If the starknet node runs on regular Docker and not balena, that is normal.\n If it runs on balena, please refresh or open a GH issue"
+                }
+                else {
+                  window.alert("Starknet Node can't stream logs from the balena supervisor. Please refresh the dashboard. If the error persists, please open a GH issue");
+                }
+                return;
+              }
+              // Enqueue the next data chunk into our target stream
+              textarea.value = String.fromCharCode.apply(null, value);
+              if (textarea != document.activeElement) {
+                textarea.scrollTop = textarea.scrollHeight;
+              }
+              return pump();
+            });
+          }
+        }
+      })
+    })
 }
